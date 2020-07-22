ce204ee02c4d0b1516c2507d4cabbb727aacf77b
Derrick Hepworth <derrickhepworth@gmail.com>
2020-07-22 16:35:00 -0600

    initial functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,13 @@
-window.alert("This is an alert! Javascript link works.")
+// initial prompt
+var playerName = window.prompt("What is your robot's name?");
+
+console.log('Your robot is ' + playerName);
+// "fight" function
+
+function fight(){
+    window.alert("The fight has begun!!");
+}
+
+//call(s)
+
+//fight();
